188fea8ee78553d168a9605b3a7f0c3265f227d7
17262270209 <2105883894@qq.com>
2026-01-08 16:30:36 +0800

    第七部分

--- djangotutorial/polls/models.py
+++ djangotutorial/polls/models.py
@@ -1,7 +1,7 @@
 # Create your models here.
 from django.db import models
 import datetime
-
+from django.contrib import admin
 from django.utils import timezone
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -9,8 +9,17 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
+    # def was_published_recently(self):
+    #     return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+    )
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 class Choice(models.Model):
